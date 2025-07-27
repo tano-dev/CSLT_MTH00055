2a0b28e10ad655357d6d18c33abc473daa995f95
tanokido85@gmail.com <tanokido85@gmail.com>
2025-07-27 14:45:16 +0700

    a

--- Bài tập - Lập trình C - Mảng.c
+++ Bài tập - Lập trình C - Mảng.c
@@ -7,6 +7,13 @@ int isPrimeNumber(int num) {
 	}
 	return 1; // Is a prime number
 }
+int sumArr(int a[], int n) {
+	int sum = 0;
+	for (int i = 0; i < n; i++) {
+		sum += a[i];
+	}
+	return sum;
+}
 float mean(int a[], int n) {
 	float sum = 0;
 	for (int i = 0; i < n; i++) {
@@ -14,28 +21,46 @@ float mean(int a[], int n) {
 	}
 	return sum / n;
 }
+int getMax(int a[], int n) {
+	int max = a[0];
+	for (int i = 1; i < n; i++) {
+		if (a[i] > max) {
+			max = a[i];
+		}
+	}
+	return max;
+}
+int getMin(int a[], int n) {
+	int min = a[0];
+	for (int i = 1; i < n; i++) {
+		if (a[i] < min) {
+			min = a[i];
+		}
+	}
+	return min;
+}
 /*
 1. Viết chương trình tính tổng các phần tử là số nguyên tố của một mảng nguyên
 */
 
-//int main() {
-//	int n;
-//	printf("Nhap so luong phan tu cua mang: ");
-//	scanf_s("%d", &n);
-//	int arr[n];
-//	printf("Nhap cac phan tu cua mang:\n");
-//	for (int i = 0; i < n; i++) {
-//		scanf_s("%d", &arr[i]);
-//	}
-//	int sum = 0;
-//	for (int i = 0; i < n; i++) {
-//		if (isPrimeNumber(arr[i])) {
-//			sum += arr[i];
-//		}
-//	}
-//	printf("Tong cac phan tu la so nguyen to: %d\n", sum);
-//	return 0;
-//}
+int main() {
+	int n;
+	printf("Nhap so luong phan tu cua mang: ");
+	scanf_s("%d", &n);
+	int arr[n];
+	printf("Nhap cac phan tu cua mang:\n");
+	for (int i = 0; i < n; i++) {
+		scanf_s("%d", &arr[i]);
+	}
+	int sum = 0;
+	for (int i = 0; i < n; i++) {
+		if (isPrimeNumber(arr[i])) {
+			sum += arr[i];
+		}
+	}
+	printf("Tong cac phan tu la so nguyen to: %d\n", sum);
+	return 0;
+}
 
 /*
 2. Viết chương trình cho phép nhập một mảng số nguyên. Sau đó, chương trình phải tính
@@ -43,25 +68,25 @@ float mean(int a[], int n) {
 lớn hơn mean của mảng
 */
 
-//int main() {
-//	int n;
-//	printf("Nhap so luong phan tu cua mang: ");
-//	scanf("%d", &n);
-//	int arr[n];
-//	printf("Nhap cac phan tu cua mang:\n");
-//	for (int i = 0; i < n; i++) {
-//		scanf("%d", &arr[i]);
-//	}
-//	float aver = mean(arr, n);
-//	int higherAver = 0;
-//	for (int i = 0; i < n; i++) {
-//		if (arr[i] > aver) {
-//			higherAver++;
-//		}
-//	}
-//	printf("Trung binh cua mang: %.2f\n", aver);
-//	printf("So phan tu lon hon trung binh: %d\n", higherAver);
-//}
+int main() {
+	int n;
+	printf("Nhap so luong phan tu cua mang: ");
+	scanf("%d", &n);
+	int arr[n];
+	printf("Nhap cac phan tu cua mang:\n");
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &arr[i]);
+	}
+	float aver = mean(arr, n);
+	int higherAver = 0;
+	for (int i = 0; i < n; i++) {
+		if (arr[i] > aver) {
+			higherAver++;
+		}
+	}
+	printf("Trung binh cua mang: %.2f\n", aver);
+	printf("So phan tu lon hon trung binh: %d\n", higherAver);
+}
 
 /*
 Giả sử cách tính thuế thu nhập cá nhân dựa trên biểu thuế như sau:
@@ -119,4 +144,108 @@ int main() {
 	printf("Tong thue thu nhap ca nhan phai dong la: %.2f trieu dong\n", tax);
 
 	return 0;
+}
+
+/*
+4. Một công ty muốn thống kê số lượng người phụ thuộc của các nhân viên. Người phụ
+thuộc của nhân viên có thể là con còn nhỏ, cha mẹ hết tuổi lao động, ... Giả sử rằng
+công ty không có nhân viên nào có quá 5 người phụ thuộc, thì một báo cáo thường có
+dạng như sau:
+Số người phụ thuộc 
+0
+1
+2
+3
+4
+5
+Số lượng nhân viên
+43
+35
+24
+11
+5
+6
+Bảng dữ liệu trên cho thấy rằng số lượng nhân viên có 2 người phụ thuộc là 24 nhân
+viên.Viết chương trình cho phép nhập lần lượt số người phụ thuộc của từng nhân viên. Số
+người phụ thuộc nhập vào phải nằm trong khoảng từ 0 đến 5, ngược lại thì chương
+trình sẽ dừng nhập rồi in ra bảng thống kê như ví dụ trên.
+*/
+
+int main() {
+	int relatives[6] = { 0 };
+	int input;
+	do {
+		scanf("%d", &input);
+		if (input < 0 || input > 5) {
+			printf("invalid.\n");
+			break;
+		}
+		relatives[input]++;
+	} while (1);
+	printf("So nguoi phu thuoc\tSo luong nhan vien\n");
+	for (int i = 0; i < 6; i++) {
+		printf("%d | %d\n", i, relatives[i]);
+	}
+	return 0;
+}
+
+/*
+5. Viết một chương trình để đếm tần suất xuất hiện của từng phần tử của một mảng. Ví
+dụ, nhập mảng {25, 12, 43, 12}, xuất ra:
+Tần suất của tất cả các phần tử của một mảng:
+25 1
+12 2
+43 1
+*/
+
+int main() {
+	int n;
+	printf("Nhap do dai cua mang: \n");
+	scanf("%d", &n);
+	int arr[n];
+	for (int i = 0; i < n; i++) {
+		printf("arr[%d]: \n",i);
+		scanf("%d", &arr[i]);
+	}
+	int maxValue = getMax(arr, n);
+	int holderArr[maxValue + 1] = {};
+	for (int i = 0; i <= maxValue; i++) holderArr[i] = 0;
+	for (int i = 0; i < n; i++) {
+		holderArr[arr[i]]++;
+	}
+	for (int i = 0; i <= maxValue; i++) {
+		if (holderArr[i] != 0) {
+			printf("%d | %d \n", i, holderArr[i]);
+		}
+	}
+}
+
+/*
+6. Viết chương trình liệt kê các dãy con không giảm của một dãy số nguyên. Ví dụ, nhập
+dãy số {2, 3, 4, 5, 5, 4, 5, 6, 7, 8, 9, 0, 1, 2, 2}, xuất ra:
+Dãy con thứ 1: 2, 3, 4, 5, 5
+Dãy con thứ 2: 4, 5, 6, 7, 8, 9
+Dãy con thứ 3: 0, 1, 2, 2
+*/
+int main() {
+	int n;
+	printf("Do dai mang: \n");
+	scanf("%d", &n);
+	int a[n];
+	for (int i = 0; i < n; i++) {
+		printf("\narr[%d] = ", i);
+		scanf("%d", &a[i]);
+	}
+	int holderArr[n] = {};
+	for (int i = 1; i < n; i++) {
+		int prevValue = a[i - 1];
+		if (prevValue > a[i]) {
+			holderArr[0] = i;
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		printf("\nholderArr[%d] = %d", i, holderArr[i]);
+	}
+	int smallArrs = sumArr(holderArr, n);
+//complete it
 }
