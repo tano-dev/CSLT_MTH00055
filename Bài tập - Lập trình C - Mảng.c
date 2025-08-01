35d948791fb95af01fc9b7e59868712f39d3c291
tanokido85@gmail.com <tanokido85@gmail.com>
2025-08-01 15:49:55 +0700

    BÀI TẬP LẬP TRÌNH C – MẢNG

--- Bài tập - Lập trình C - Mảng.c
+++ Bài tập - Lập trình C - Mảng.c
@@ -21,6 +21,11 @@ float mean(int a[], int n) {
 	}
 	return sum / n;
 }
+void initArr(int a[], int n) {
+	for (int i = 0; i < n; i++) {
+		a[i] = 0;
+	}
+}
 int getMax(int a[], int n) {
 	int max = a[0];
 	for (int i = 1; i < n; i++) {
@@ -273,4 +278,175 @@ int main() {
 			}
 		}
 	}
+}
+
+/*
+7. Viết chương trình liệt kê các dãy con không lặp của một dãy số nguyên. Ví dụ, nhập
+dãy số {2, 3, 4, 5, 5, 4, 5, 6, 7, 8, 9, 0, 1, 2, 2}, xuất ra:
+Dãy con thứ 1: 2, 3, 4, 5
+Dãy con thứ 2: 5, 4
+Dãy con thứ 3: 5, 6, 7, 8, 9, 0, 1, 2
+Dãy con thứ 4: 2
+
+*/
+
+int main() {
+	int n;
+	printf("Do dai mang: \n");
+	scanf("%d", &n);
+	int a[n];
+	for (int i = 0; i < n; i++) {
+		printf("\narr[%d] = ", i);
+		scanf("%d", &a[i]);
+	}
+	int holderArr[n] = {};
+	int index = 0;
+	int numberCheckArr[getMax(a, n)] = {};
+	initArr(numberCheckArr, getMax(a, n));
+	for (int i = 0; i < n; i++) {
+		if (numberCheckArr[a[i]] == 1) {
+			holderArr[index] = i;
+			index++;
+			initArr(numberCheckArr, getMax(a, n)); // Reset the number check array
+		}
+		numberCheckArr[a[i]]++;
+	}
+	 //for (int i = 0; i < n; i++) {
+ 	//	printf("\nholderArr[%d] = %d", i, holderArr[i]);
+ 	//}
+		for (int i = 0; i <= index; i++) {
+			printf("\nDay con thu %d: ", i + 1);
+			if (i == 0) {
+				for (int j = 0; j < holderArr[i]; j++) {
+					printf("%d, ", a[j]);
+				}
+			}
+			else if (i == index) {
+				for (int j = holderArr[i - 1]; j < n; j++) {
+					printf("%d, ", a[j]);
+				}
+			}
+			else {
+				for (int j = holderArr[i - 1]; j < holderArr[i]; j++) {
+					printf("%d, ", a[j]);
+				}
+			}
+		}
+}
+
+/*
+8. Trong một dãy số nguyên, phần tử cực đại là phần tử lớn hơn hai phần tử liền kề. Viết
+chương trình tìm phần tử cực đại lớn nhất của một dãy số nguyên. Ví dụ, nhập dãy số
+{2, 7, 12, 25, 4, 57, 27, 44}, xuất ra:
+Phần tử cực đại lớn nhất là: 57
+
+8
+2 7 12 25 4 57 27 44
+*/
+
+int main() {
+	int n;
+	printf("Do dai mang: \n");
+	scanf("%d", &n);
+	int a[n];
+	for (int i = 0; i < n; i++) {
+		printf("\narr[%d] = ", i);
+		scanf("%d", &a[i]);
+	}
+	int maxValue = a[0];
+	for (int i = 1; i < n - 1; i++) {
+		if (a[i] > a[i - 1] && a[i] > a[i + 1]) {
+			if (a[i] > maxValue) {
+				maxValue = a[i];
+			}
+		}
+	}
+
+	printf("Phan tu cuc dai lon nhat la: %d\n", maxValue);
+	return 0;
+}
+
+/*
+9. Cho một mảng nguyên A[n] và một số nguyên k. Viết chương trình để:
+- Duyệt mảng A từ phần tử đầu tiên, nếu A[i] chia hết cho k thì thêm k phần tử A[i]/k
+vào mảng A.
+- Tính tổng của các phần tử có thể có của A.
+Ví dụ, nhập mảng A = {4, 6, 2} thì các phần tử của A sau khi duyệt mảng là {4, 6, 2,
+2, 2, 3, 3, 1, 1, 1, 1, 1, 1}, xuất ra:
+Tổng của các phần tử của mảng sau khi duyệt là: 28
+
+
+
+*/
+
+// đề ngu, đọc dell hiểu, dell làm. Hết.
+
+/*
+10. Viết chương trình sao chép mảng hai chiều sang mảng một chiều
+*/
+
+int main() {
+	int n, m;
+	printf("Nhap so dong cua mang: ");
+	scanf("%d", &n);
+	printf("Nhap so cot cua mang: ");
+	scanf("%d", &m);
+	int arr2D[n][m];
+	printf("Nhap cac phan tu cua mang:\n");
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			scanf("%d", &arr2D[i][j]);
+		}
+	}
+	int arr1D[n * m];
+	int index = 0;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			arr1D[index++] = arr2D[i][j];
+		}
+	}
+	printf("Mang mot chieu sau khi sao chep:\n");
+	for (int i = 0; i < n * m; i++) {
+		printf("%d ", arr1D[i]);
+	}
+	return 0;
+}
+/*
+
+11. Viết chương trình nhập vào một ma trận vuông (có giá trị nguyên), kích thước do người
+dùng nhập vào. Sau đó, kiểm tra ma trận đó có đối xứng qua đường chéo chính hay
+không.
+
+*/
+
+#include <stdio.h>
+int main() {
+	int n;
+	printf("Nhap kich thuoc ma tran vuong: ");
+	scanf("%d", &n);
+	int matrix[n][n];
+
+	printf("Nhap cac phan tu cua ma tran:\n");
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			scanf("%d", &matrix[i][j]);
+		}
+	}
+	int isSymmetric = 1; // Giả sử ma trận là đối xứng
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (matrix[i][j] != matrix[j][i]) {
+				isSymmetric = 0; // Nếu có phần tử không bằng, không đối xứng
+				break;
+			}
+		}
+		if (!isSymmetric) break;
+	}
+	if (isSymmetric) {
+		printf("Ma tran la doi xung qua duong cheo chinh.\n");
+	}
+	else {
+		printf("Ma tran khong doi xung qua duong cheo chinh.\n");
+	}
+	return 0;
 }
