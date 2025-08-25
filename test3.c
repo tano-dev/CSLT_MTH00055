f883e8d80ce439dceaf8db1a14d07f68213586e8
tano (lap) <tanokido854@gmail.com>
2025-08-25 11:07:17 +0700

    test 3

---
+++ test3.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+// Additional Functions
+int isPrimeNumber(int num) {
+	if (num < 2) return 0; // 0 and 1 are not prime numbers
+	for (int i = 2; i * i <= num; i++) {
+		if (num % i == 0) return 0; // Not a prime number
+	}
+	return 1; // Is a prime number
+}
+int sumArr(int a[], int n) {
+	int sum = 0;
+	for (int i = 0; i < n; i++) {
+		sum += a[i];
+	}
+	return sum;
+}
+float mean(int a[], int n) {
+	float sum = 0;
+	for (int i = 0; i < n; i++) {
+		sum += a[i];
+	}
+	return sum / n;
+}
+void initArr(int a[], int n) {
+	for (int i = 0; i < n; i++) {
+		a[i] = 0;
+	}
+}
+int getMax(int a[], int n) {
+	int max = a[0];
+	for (int i = 1; i < n; i++) {
+		if (a[i] > max) {
+			max = a[i];
+		}
+	}
+	return max;
+}
+int getMin(int a[], int n) {
+	int min = a[0];
+	for (int i = 1; i < n; i++) {
+		if (a[i] < min) {
+			min = a[i];
+		}
+	}
+	return min;
+}
+int arrLength(int a[]) {
+	return sizeof(a) / sizeof(a[0]);
+}
